90adfb3f87623bc677d90df9551190691d4af099
Esteban Restrepo <erestrepo@webcol.net>
2022-07-07 19:21:02 -0500

    sucursales

---
+++ vistasProyect/src/app/components/usuarios/sucursales/interface/sucursales.interface.ts
@@ -0,0 +1,6 @@
+export interface persona {
+    nombre: String;
+    correo: String;
+    contrasena: String;
+    rol: String;
+}
--- vistasProyect/src/app/components/usuarios/sucursales/sucursales.component.ts
+++ vistasProyect/src/app/components/usuarios/sucursales/sucursales.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { persona } from './interface/sucursales.interface';
 
 @Component({
   selector: 'app-sucursales',
   templateUrl: './sucursales.component.html',
   styleUrls: ['./sucursales.component.css']
 })
-export class SucursalesComponent implements OnInit {
+export class SucursalesComponent {
+  
+  public datosPersonas: persona[] = [
+    {
+      nombre: 'christopher bang',
+      correo: 'usuario@tacabell.com',
+      contrasena: 'i4w1011i',
+      rol: 'administrador',
+    },
+    {
+      nombre: 'Andy Ackerman',
+      correo: 'usuario@tacabell.com',
+      contrasena: 'i4w1011i',
+      rol: 'administrador',
+    },
+    {
+      nombre: 'Yujii Itadoria',
+      correo: 'usuario@tacabell.com',
+      contrasena: 'i4w1011i',
+      rol: 'administrador',
+    },
+    {
+      nombre: 'Eren Jaeger',
+      correo: 'usuario@tacabell.com',
+      contrasena: 'i4w1011i',
+      rol: 'administrador',
+    },
+    {
+      nombre: 'Armin Arlert',
+      correo: 'usuario@tacabell.com',
+      contrasena: 'i4w1011i',
+      rol: 'administrador',
+    },
+    {
+      nombre: 'Annie Leonhart',
+      correo: 'usuario@tacabell.com',
+      contrasena: 'i4w1011i',
+      rol: 'administrador',
+    },
+    {
+      nombre: 'Reiner Braun',
+      correo: 'usuario@tacabell.com',
+      contrasena: 'i4w1011i',
+      rol: 'administrador',
+    },
+    {
+      nombre: 'Erwin Smith',
+      correo: 'usuario@tacabell.com',
+      contrasena: 'i4w1011i',
+      rol: 'administrador',
+    },
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  ];
 
 }
